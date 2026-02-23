d74c93f43b2d90fceab4cdd4366449fa9158f842
minseokim0113 <minseokim0113@gmail.com>
2026-02-23 19:54:06 +0900

    [level 4] Title: 징검다리, Time: 29.07 ms, Memory: 98 MB -BaekjoonHub

---
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int distance, int[] rocks, int n) {
+        Arrays.sort(rocks);
+        int answer = 0;
+        
+        int start = 1;
+        int end = distance;
+        
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            int prev = 0;
+            int cnt = 0;
+            
+            for(int i = 0; i < rocks.length; i++) {
+                if(rocks[i] - prev < mid) {
+                    cnt++;
+                } else {
+                    prev = rocks[i];
+                }
+            }
+            
+            if(distance - prev < mid) {
+                cnt++;
+            }
+            
+            if(cnt <= n) {
+                answer = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        return answer;
+    }
+}
