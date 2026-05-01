69be3cb833c693fccfef4af2da673a79be14a00e
minseokim0113 <minseokim0113@gmail.com>
2026-05-01 23:43:00 +0900

    [level 2] Title: 전화번호 목록, Time: 342.33 ms, Memory: 99.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        
+        for(int i = 0; i < phone_book.length - 1; i++) {
+            String num = phone_book[i];
+            
+            if(phone_book[i + 1].startsWith(num)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
