062e011054d6958efd47398f64176338172b09df
minseokim0113 <minseokim0113@gmail.com>
2026-05-12 23:45:44 +0900

    [level 2] Title: 피로도, Time: 1.67 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,25 @@
+class Solution {
+    
+    static int answer = 0;
+    static boolean[] visited;
+    
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        
+        dfs(k, dungeons, 0);
+        
+        return answer;
+    }
+    
+    void dfs(int fatigue, int[][] dungeons, int count) {
+        answer = Math.max(answer, count);
+        
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && fatigue >= dungeons[i][0]) {
+                visited[i] = true;
+                dfs(fatigue - dungeons[i][1], dungeons, count + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
