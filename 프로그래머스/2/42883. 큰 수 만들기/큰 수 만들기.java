f4ca097d9f43908791d0f1f895a95f58bcbea3e8
minseokim0113 <minseokim0113@gmail.com>
2026-06-13 23:52:10 +0900

    [level 2] Title: 큰 수 만들기, Time: 52.52 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String number, int k) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : number.toCharArray()) {
+
+            while (!stack.isEmpty()
+                    && k > 0
+                    && stack.peek() < c) {
+
+                stack.pop();
+                k--;
+            }
+
+            stack.push(c);
+        }
+
+        while (k > 0) {
+            stack.pop();
+            k--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : stack) {
+            sb.append(c);
+        }
+
+        return sb.toString();
+    }
+}
