fe88e3c24606a6e964d9ceb7a1ccb449cb4b747d
minseokim0113 <minseokim0113@gmail.com>
2026-06-22 19:40:50 +0900

    [level 2] Title: 짝지어 제거하기, Time: 57.36 ms, Memory: 68.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        
+        if (!stack.isEmpty()) {
+            return 0;  
+        }
+        
+        return 1;
+    }
+}
