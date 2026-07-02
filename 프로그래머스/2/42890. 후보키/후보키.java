2eece71190743bb5c969add67c1053ce75593c30
minseokim0113 <minseokim0113@gmail.com>
2026-07-02 16:52:10 +0900

    [level 2] Title: 후보키, Time: 4.68 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] relation) {
+        
+        int row = relation.length;
+        int col = relation[0].length;
+        List<Integer> candidateKeys = new ArrayList<>();
+
+        for (int mask = 1; mask < (1 << col); mask++) {
+
+            Set<String> set = new HashSet<>();
+
+            for (int r = 0; r < row; r++) {
+
+                StringBuilder sb = new StringBuilder();
+
+                for (int c = 0; c < col; c++) {
+
+                    if ((mask & (1 << c)) != 0) {
+                        sb.append(relation[r][c]).append(",");
+                    }
+
+                }
+
+                set.add(sb.toString());
+            }
+
+            if (set.size() == row) {
+                boolean isMinimal = true;
+
+                for (int key : candidateKeys) {
+
+                    if ((key & mask) == key) {
+                        isMinimal = false;
+                        break;
+                    }
+                }
+                
+                if (isMinimal) {
+                    candidateKeys.add(mask);
+                }
+            }
+        }
+        
+        return candidateKeys.size();
+    }
+}
