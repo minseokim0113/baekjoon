e582e2b413ce9325512f97aa22b69b0a0840109a
minseokim0113 <minseokim0113@gmail.com>
2026-07-14 15:09:58 +0900

    [level 2] Title: 주식가격, Time: 8.57 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,20 +1,18 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] prices) {
-        
         int[] answer = new int[prices.length];
         
         for(int i = 0; i < prices.length; i++) {
             int sec = 1;
             
-            for (int  j = i + 1; j < prices.length; j++) {
+            for (int j = i + 1; j < prices.length; j++) {
                 
-                if(j == prices.length - 1) {
+                if (j == prices.length - 1) {
                     answer[i] = sec;
                 } 
                 
-                if(prices[i] <= prices[j]) {
+                
+                if (prices[j] >= prices[i]) {
                     sec++;
                 } else {
                     answer[i] = sec;
